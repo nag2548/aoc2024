0308792bc4ac925c69ea4288d29f49d2ecd1760d
nag2548 <nadine.grabmair@gmx.de>
2024-11-26 22:53:04 +0100

    🎉 init project

---
+++ src/test/java/de/zimtix/MainTest.java
@@ -0,0 +1,18 @@
+package de.zimtix;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * A simple unit test
+ */
+public class MainTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
