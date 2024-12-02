aab0c26b730b81e2580f3df9f1babff22233919a
nag2548 <nadine.grabmair@gmx.de>
2024-12-02 16:13:21 +0100

    🚧 (day3): setup

---
+++ src/main/java/de/zimtix/aoc2024/day3/Day3.java
@@ -0,0 +1,11 @@
+package de.zimtix.aoc2024.day3;
+
+import de.zimtix.aoc2024.Puzzle;
+
+import java.util.List;
+
+public abstract class Day3 extends Puzzle {
+    public Day3(List<String> lines) {
+        super(lines);
+    }
+}
