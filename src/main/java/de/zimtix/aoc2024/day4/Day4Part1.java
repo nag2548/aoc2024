d3b12da810930f8c88556d852fd8444c2b41f877
Nadine Grabmair <nadine.grabmair@gmx.de>
2024-12-03 22:12:43 +0100

    🚧 (day4): setup

---
+++ src/main/java/de/zimtix/aoc2024/day4/Day4Part1.java
@@ -0,0 +1,14 @@
+package de.zimtix.aoc2024.day4;
+
+import java.util.List;
+
+public class Day4Part1 extends Day4 {
+    public Day4Part1(List<String> lines) {
+        super(lines);
+    }
+
+    @Override
+    public Object getResult() {
+        return null;
+    }
+}
