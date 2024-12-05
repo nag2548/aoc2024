1808629ce8783b7385cd591fed013151ea70925d
Nadine Grabmair <nadine.grabmair@gmx.de>
2024-12-05 19:49:20 +0100

    🚧 (day6): setup

---
+++ src/main/java/de/zimtix/aoc2024/day6/Day6Part2.java
@@ -0,0 +1,14 @@
+package de.zimtix.aoc2024.day6;
+
+import java.util.List;
+
+public class Day6Part2 extends Day6 {
+    public Day6Part2(List<String> lines) {
+        super(lines);
+    }
+
+    @Override
+    public Object getResult() {
+        return null;
+    }
+}
---
+++ src/test/java/de/zimtix/aoc2024/day6/Day6Part1Tests.java
@@ -0,0 +1,33 @@
+package de.zimtix.aoc2024.day6;
+
+import de.zimtix.aoc2024.Puzzle;
+import de.zimtix.aoc2024.PuzzleTest;
+
+import java.util.List;
+
+class Day6Part1Tests extends PuzzleTest {
+    @Override
+    protected String getSampleInputFile() {
+        return "day6/sample.txt";
+    }
+
+    @Override
+    protected String getRealInputFile() {
+        return "day6/real.txt";
+    }
+
+    @Override
+    protected Object getExpectedSampleResult() {
+        return -1;
+    }
+
+    @Override
+    protected Object getExpectedRealResult() {
+        return -1;
+    }
+
+    @Override
+    protected Puzzle getComponent(List<String> lines) {
+        return new Day6Part1(lines);
+    }
+}
