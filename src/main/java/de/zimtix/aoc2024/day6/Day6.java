fcff4676dcde70b573223a237f7bc7b0888e680c
Nadine Grabmair <nadine.grabmair@gmx.de>
2024-12-05 19:49:20 +0100

    🚧 (day6): setup

---
+++ src/main/java/de/zimtix/aoc2024/day6/Day6.java
@@ -0,0 +1,11 @@
+package de.zimtix.aoc2024.day6;
+
+import de.zimtix.aoc2024.Puzzle;
+
+import java.util.List;
+
+public abstract class Day6 extends Puzzle {
+    public Day6(List<String> lines) {
+        super(lines);
+    }
+}
---
+++ src/main/java/de/zimtix/aoc2024/day6/Day6Part1.java
@@ -0,0 +1,14 @@
+package de.zimtix.aoc2024.day6;
+
+import java.util.List;
+
+public class Day6Part1 extends Day6 {
+    public Day6Part1(List<String> lines) {
+        super(lines);
+    }
+
+    @Override
+    public Object getResult() {
+        return null;
+    }
+}
