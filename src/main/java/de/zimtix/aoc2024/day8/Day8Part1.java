acf67608ff6c084b7e7029d6ce786e65d190ef99
nag2548 <nadine.grabmair@gmx.de>
2024-12-07 16:23:56 +0100

    🚧 ️(day8): setup

---
+++ src/main/java/de/zimtix/aoc2024/day8/Day8Part1.java
@@ -0,0 +1,14 @@
+package de.zimtix.aoc2024.day8;
+
+import java.util.List;
+
+public class Day8Part1 extends Day8 {
+    public Day8Part1(List<String> lines) {
+        super(lines);
+    }
+
+    @Override
+    public Object getResult() {
+        return null;
+    }
+}
---
+++ src/main/java/de/zimtix/aoc2024/day8/Day8Part2.java
@@ -0,0 +1,14 @@
+package de.zimtix.aoc2024.day8;
+
+import java.util.List;
+
+public class Day8Part2 extends Day8 {
+    public Day8Part2(List<String> lines) {
+        super(lines);
+    }
+
+    @Override
+    public Object getResult() {
+        return null;
+    }
+}
