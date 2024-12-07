8363dc9311d06f596c1db2bc44dc3b7fe03f6c3a
nag2548 <nadine.grabmair@gmx.de>
2024-12-07 16:23:56 +0100

    🚧 ️(day8): setup

---
+++ src/main/java/de/zimtix/aoc2024/day8/Day8.java
@@ -0,0 +1,11 @@
+package de.zimtix.aoc2024.day8;
+
+import de.zimtix.aoc2024.Puzzle;
+
+import java.util.List;
+
+public abstract class Day8 extends Puzzle {
+    public Day8(List<String> lines) {
+        super(lines);
+    }
+}
---
+++ src/test/java/de/zimtix/aoc2024/day8/Day8Part2Tests.java
@@ -0,0 +1,33 @@
+package de.zimtix.aoc2024.day8;
+
+import de.zimtix.aoc2024.Puzzle;
+import de.zimtix.aoc2024.PuzzleTest;
+
+import java.util.List;
+
+class Day8Part2Tests extends PuzzleTest {
+    @Override
+    protected String getSampleInputFile() {
+        return "day8/sample.txt";
+    }
+
+    @Override
+    protected String getRealInputFile() {
+        return "day8/real.txt";
+    }
+
+    @Override
+    protected Object getExpectedSampleResult() {
+        return null;
+    }
+
+    @Override
+    protected Object getExpectedRealResult() {
+        return null;
+    }
+
+    @Override
+    protected Puzzle getComponent(List<String> lines) {
+        return new Day8Part2(lines);
+    }
+}
