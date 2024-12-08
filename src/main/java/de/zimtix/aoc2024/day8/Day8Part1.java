8d79a82dab0df9ab4463999893b87b98659b2a4f
nag2548 <nadine.grabmair@gmx.de>
2024-12-08 15:06:02 +0100

    ✨ (day8): both parts

--- src/main/java/de/zimtix/aoc2024/day8/Day8Part1.java
+++ src/main/java/de/zimtix/aoc2024/day8/Day8Part1.java
@@ -1,6 +1,7 @@
 package de.zimtix.aoc2024.day8;
 
 import java.util.List;
+import java.util.function.BiFunction;
 
 public class Day8Part1 extends Day8 {
     public Day8Part1(List<String> lines) {
@@ -8,7 +9,8 @@ public class Day8Part1 extends Day8 {
     }
 
     @Override
-    public Object getResult() {
-        return null;
+    protected void collectAntinodes(Day8Coordinate coordinate, int x, int y, BiFunction<Integer, Integer, Integer> operation) {
+        Day8Coordinate newCoordinate = new Day8Coordinate(operation.apply(coordinate.x(), x), operation.apply(coordinate.y(), y));
+        putIfInBounds(newCoordinate);
     }
 }
