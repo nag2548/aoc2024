531009c29477edbb1d1b416bb3abdef8d3be272f
nag2548 <nadine.grabmair@gmx.de>
2024-12-08 15:22:47 +0100

    🚧 (day9): setup

---
+++ src/main/java/de/zimtix/aoc2024/day9/Day9.java
@@ -0,0 +1,11 @@
+package de.zimtix.aoc2024.day9;
+
+import de.zimtix.aoc2024.Puzzle;
+
+import java.util.List;
+
+public abstract class Day9 extends Puzzle {
+    public Day9(List<String> lines) {
+        super(lines);
+    }
+}
---
+++ src/main/java/de/zimtix/aoc2024/day9/Day9Part1.java
@@ -0,0 +1,14 @@
+package de.zimtix.aoc2024.day9;
+
+import java.util.List;
+
+public class Day9Part1 extends Day9 {
+    public Day9Part1(List<String> lines) {
+        super(lines);
+    }
+
+    @Override
+    public Object getResult() {
+        return null;
+    }
+}
