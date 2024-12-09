a4333f95ea9eb284aa78dbe6f0c2996398a7f661
nag2548 <nadine.grabmair@gmx.de>
2024-12-10 00:19:32 +0100

    ✨ (day10): setup

---
+++ src/main/java/de/zimtix/aoc2024/day10/Day10Part2.java
@@ -0,0 +1,14 @@
+package de.zimtix.aoc2024.day10;
+
+import java.util.List;
+
+public class Day10Part2 extends Day10 {
+    public Day10Part2(List<String> lines) {
+        super(lines);
+    }
+
+    @Override
+    public Object getResult() {
+        return null;
+    }
+}
---
+++ src/test/java/de/zimtix/aoc2024/day10/Day10Part1Tests.java
@@ -0,0 +1,33 @@
+package de.zimtix.aoc2024.day10;
+
+import de.zimtix.aoc2024.Puzzle;
+import de.zimtix.aoc2024.PuzzleTest;
+
+import java.util.List;
+
+class Day10Part1Tests extends PuzzleTest {
+    @Override
+    protected String getSampleInputFile() {
+        return "day10/sample.txt";
+    }
+
+    @Override
+    protected String getRealInputFile() {
+        return "day10/real.txt";
+    }
+
+    @Override
+    protected Object getExpectedSampleResult() {
+        return -1;
+    }
+
+    @Override
+    protected Object getExpectedRealResult() {
+        return -1;
+    }
+
+    @Override
+    protected Puzzle getComponent(List<String> lines) {
+        return new Day10Part1(lines);
+    }
+}
