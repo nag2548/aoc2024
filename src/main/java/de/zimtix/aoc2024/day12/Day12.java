376f45a85c68f95a7b44ccafb04abf2d9f7777b6
nag2548 <nadine.grabmair@gmx.de>
2024-12-12 18:46:41 +0100

    ✨ (day12): part 1

---
+++ src/main/java/de/zimtix/aoc2024/day12/Day12.java
@@ -0,0 +1,11 @@
+package de.zimtix.aoc2024.day12;
+
+import de.zimtix.aoc2024.Puzzle;
+
+import java.util.List;
+
+public abstract class Day12 extends Puzzle {
+    public Day12(List<String> lines) {
+        super(lines);
+    }
+}
---
+++ src/main/java/de/zimtix/aoc2024/day12/Day12Coordinate.java
@@ -0,0 +1,4 @@
+package de.zimtix.aoc2024.day12;
+
+public record Day12Coordinate(int x, int y) {
+}
