c1c1f252768babd8e25954dcf73b4de50d1d34f7
nag2548 <nadine.grabmair@gmx.de>
2024-12-13 17:12:01 +0100

    🚧 (day14): setup

---
+++ src/main/java/de/zimtix/aoc2024/day14/Day14.java
@@ -0,0 +1,11 @@
+package de.zimtix.aoc2024.day14;
+
+import de.zimtix.aoc2024.Puzzle;
+
+import java.util.List;
+
+public abstract class Day14 extends Puzzle {
+    public Day14(List<String> lines) {
+        super(lines);
+    }
+}
