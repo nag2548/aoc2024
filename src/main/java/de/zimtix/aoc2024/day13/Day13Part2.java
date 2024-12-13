bb181a12404f5c7addbcec7d6408ab0a41c29749
nag2548 <nadine.grabmair@gmx.de>
2024-12-13 17:03:12 +0100

    ✨ (day13): part 2

--- src/main/java/de/zimtix/aoc2024/day13/Day13Part2.java
+++ src/main/java/de/zimtix/aoc2024/day13/Day13Part2.java
@@ -2,14 +2,14 @@ package de.zimtix.aoc2024.day13;
 
 import java.util.List;
 
-public class Day13Part1 extends Day13 {
-    public Day13Part1(List<String> lines) {
+public class Day13Part2 extends Day13 {
+    public Day13Part2(List<String> lines) {
         super(lines);
     }
 
     @Override
     public Object getResult() {
-        init(0L);
+        init(10000000000000L);
         return getTokens();
     }
 }
